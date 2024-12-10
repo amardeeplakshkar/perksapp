c589452d6c241068c6c1af832ed34c0cf4e66158
Amardeep Lakshkar <info.amardeeppvt@gmail.com>
2024-12-10 19:22:45 +0530

    Added: Ton Connect UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import { ScrollArea } from "../components/ui/scroll-area";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-black flex w-full flex-col overflow-hidden">
+      <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
         <ScrollArea className="flex-1">
           <TonConnectUIProvider manifestUrl="https://apricot-selected-dog-88.mypinata.cloud/ipfs/QmXvaFWgecTknsxyq4zASJSHMCqKJ3vXz2KgFnejC6oGHu">
             {children}
