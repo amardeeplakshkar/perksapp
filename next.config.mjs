3fcb18c3849419ce0f482af3747a3d93f682c039
Amardeep Lakshkar <info.amardeeppvt@gmail.com>
2024-12-17 10:26:34 +0530

    Added: shop

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: { // [!code ++] // [!code focus]
+    serverComponentsExternalPackages: ['grammy'], // [!code ++] // [!code focus]
+  },
     images: {
       remotePatterns: [
         {
